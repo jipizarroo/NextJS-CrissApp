0a5f9cd5d7820b0a411a252dd2bd1404bec193fc
Javier Pizarro <jipizarroo@gmail.com>
2023-02-13 22:54:16 -0300

    fix(error): better way of handling error now

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,3 +1,18 @@
+export function createApiError(
+  message: string,
+  status: number,
+  response: Response
+) {
+  return {
+    message,
+    status,
+    response,
+    toString() {
+      return `ApiError: ${this.message} (status: ${this.status})`
+    },
+  }
+}
+
 export default function fetcher(url: string, data = undefined) {
   return fetch(`${window.location.origin}/api${url}`, {
     method: data ? 'POST' : 'GET',
@@ -6,10 +21,26 @@ export default function fetcher(url: string, data = undefined) {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(data),
-  }).then((res) => {
-    if (res.status > 399 || res.status < 200) {
-      throw new Error(`error ${res.status}`)
-    }
-    return res.json()
   })
+    .then((res) => {
+      if (res.status === 400) {
+        throw createApiError('Bad Request', res.status, res)
+      } else if (res.status === 401) {
+        throw createApiError('Unauthorized', res.status, res)
+      } else if (res.status === 404) {
+        throw createApiError('Not Found', res.status, res)
+      } else if (res.status === 500) {
+        throw createApiError('Internal Server Error', res.status, res)
+      } else if (res.status > 399 || res.status < 200) {
+        throw createApiError(`Error ${res.status}`, res.status, res)
+      }
+      return res.json()
+    })
+    .catch((error) => {
+      // eslint-disable-next-line no-console
+      console.error(error)
+      // eslint-disable-next-line no-alert
+      window.alert(error.message)
+      throw error
+    })
 }
